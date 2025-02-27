e28b638405f2aead4ee0b7077553c0e142c68621
milligang <milligan.grinstead@gmail.com>
2025-02-27 14:38:19 -0500

    set up

---
+++ graphs.c
@@ -0,0 +1,99 @@
+/*
+def graph_basic_no_g(n):
+    # output: number of vertices and weights dict
+    weight = {}
+    # decide edges to ignore
+    cut_off = float('inf')
+    if n > 3:
+        cut_off = 20*(1/(n-1)-1/((n-1)**2))
+    # edge from every v to every w except itself, add random weight
+    for v in range(n):
+        for edge in range(v+1,n):
+            w = random.uniform(0, 1)
+            if w < cut_off:
+                weight[(v,edge)] = w
+    return weight
+
+def hypercube_no_g(n):
+    # output: number of vertices and weights dict
+    weight = {}
+    cut_off = 0.45 + 1/(n**(1/4))
+    for i in range(n):
+        for j in range(i+1,n):
+            #print(abs(i - j))
+                w = np.random.uniform(0, 1)
+                if w < cut_off:
+                    if math.log(abs(i - j), 2).is_integer():
+                        weight[(i,j)] = w
+    return weight
+
+def uniformly_no_g(n):
+    # output: number of vertices and weights dict
+    weight = {}
+    points = np.zeros((n,2))
+    # remove large edges
+    cut_off = 3/(n**(1/2))
+    for i in range(n):
+        x = np.random.uniform(0, 1)
+        y = np.random.uniform(0, 1)
+        points[i] = x,y
+    for i in range(n):
+        (x1,y1) = points[i]
+        for j in range(i+1, n):
+            x2,y2 = points[j]
+            dist = np.sqrt((x2-x1)**2+(y2-y1)**2)
+            if dist < cut_off:
+                weight[(i,j)] = dist
+    return weight
+                                
+def graph_cube3_no_g(n):
+    # output: number of vertices and weights dict
+    weight = {}
+    points = np.zeros((n,3))
+    # decide edges to delete
+    cut_off = 2.2/(2**(math.log(n,8)))
+    # np.sqrt(3)/math.log(n,5)
+    #3/(2**(math.log(n,4)))
+    for i in range(n):
+        x = np.random.uniform(0, 1)
+        y = np.random.uniform(0, 1)
+        z = np.random.uniform(0, 1)
+        points[i] = (x,y,z)
+    for i in range(n):
+        (x1,y1,z1) = points[i]
+        for j in range(i+1,n):
+            (x2,y2,z2) = points[j]
+            dist = np.sqrt((x2-x1)**2+(y2-y1)**2+(z2-z1)**2)
+            if dist < cut_off:
+                weight[(i,j)] = dist
+    return weight
+
+def graph_cube4_no_g(n):
+    # output: number of vertices and weights dict
+    weight = {}
+    points = np.zeros((n,4))
+    cut_off = 1.7/(n**(1/4))
+    # make graph
+    w1 = np.random.uniform(0, 1)
+    x1 = np.random.uniform(0, 1)
+    y1 = np.random.uniform(0, 1)
+    z1 = np.random.uniform(0, 1)
+    points[0] = (w1,x1,y1,z1)
+    for j in range(1, n):
+        w2 = np.random.uniform(0, 1)
+        x2 = np.random.uniform(0, 1)
+        y2 = np.random.uniform(0, 1)
+        z2 = np.random.uniform(0, 1)
+        points[j] = (w2,x2,y2,z2)
+        dist = np.sqrt((w2-w1)**2+(x2-x1)**2+(y2-y1)**2+(z2-z1)**2)
+        if dist < cut_off:
+            weight[(0,j)] = dist
+    for i in range(1,n):
+        (w1,x1,y1,z1) = points[i]
+        for j in range(i+1, n):
+            (w2,x2,y2,z2) = points[j]
+            dist = np.sqrt((w2-w1)**2+(x2-x1)**2+(y2-y1)**2+(z2-z1)**2)
+            if dist < cut_off:
+                weight[(i,j)] = dist
+    return weight
+*/
--- randmst.c
+++ randmst.c
@@ -1,5 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//gcc -Wall randmst.c -o randmst
+//input to command line: ./randmst 0 numpoints numtrials dimension
+
 int main(int argc, char *argv[]){
     // argc = number of command line args
-    int flag = argv[1];
+    int flag = atoi(argv[1]);
+    int numpoints = atoi(argv[2]);
+    int numtrials = atoi(argv[2]);
+    int dimension = atoi(argv[2]);
+    printf("%d", 1);
     return 0;
 }
+/*
+def avg_weight(dim, n, trials):
+    type_graph = graph_fxns[dim]
+    avg = 0
+    for _ in range(trials):
+    w = type_graph(n)
+    mstweight = algs.kruskals_no_g(n,w)
+    avg += mstweight
+    return (avg / trials)
+
+avg = avg_weight(alg_flag, dimension, numpoints, numtrials)
+print(avg, numpoints, numtrials, dimension)
+*/
